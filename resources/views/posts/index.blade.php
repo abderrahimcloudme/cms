34c4628983f46e64fa40de11d964fa48cb7072b5
abderrahim cloudme <abderrahim.cloudme@gmail.com>
2019-11-12 21:07:27 +0100

    fetch posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,7 +10,24 @@
 <div class="card card-default">
 	<div class="card-header">Posts</div>
 	<div class="card-body">
-		
+		<table class="table">
+			<thead>
+				<th>Image</th>
+				<th>Titre</th>
+				<th></th>
+				<th></th>
+			</thead>
+			<tbody>
+				@foreach($posts as $post)
+					<tr>
+						<td><img src="{{ asset($post->image) }}" alt=""></td>
+						<td>{{ $post->title }}</td>
+						<td></td>
+						<td></td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
 	</div>
 </div>
 
