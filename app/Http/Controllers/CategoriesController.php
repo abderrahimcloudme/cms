d4b7573c22e5d5777e3b071b172aa11af34cdb88
ismou1337 <ismail.oukalla@gmail.com>
2019-11-21 20:23:26 +0100

    Delete buttons and controllers

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -102,8 +102,10 @@ class CategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        session()->flash('success', 'Category has been deleted');
+        return redirect(route('categories.index'));
     }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -32,6 +32,7 @@
 
 	<td>
 		<a href="{{ route('categories.edit', $category->id) }}" class="btn-info btn-sm">Edit </a>
+		<button class="btn btn-danger btn-sm" onclick="handleDelete({{$category->id}})">Delete</button>
 	</td>
 </tr>
 @endforeach
@@ -40,7 +41,44 @@
 	</div>
 
 </div>
+  <!-- Modal -->
+  
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollable" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-scrollable" role="document">
+				<form action="" method="POST" id="handleDelete">
+		  <div class="modal-content">
+			  @csrf
+			  @method('DELETE')
+			<div class="modal-header">
+			  <h5 class="modal-title" id="exampleModalScrollableTitle">Delete</h5>
+			  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			  </button>
+			</div>
+			<div class="modal-body">
+			  Do you want to delete this?
+			</div>
+			<div class="modal-footer">
+			  <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Please</button>
+			  <button type="submit" class="btn btn-danger">Yes, Please</button>
+			</div>
+		  </div>
+		</form>
+		</div>
+	  </div>
+
 
 @endsection
 
+@section('script')
+<script>
+function handleDelete(id){
 
+	$('#exampleModalScrollable').modal('show')
+	var form=document.getElementById('handleDelete');
+	form.action = '/categories/'+ id;
+	console.log(form);
+
+}
+</script>
+@endsection
